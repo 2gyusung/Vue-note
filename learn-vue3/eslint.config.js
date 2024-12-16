396fa832743257f7fbd27c0d9071e5dc18202417
이규성 <leegs005@naver.com>
2024-12-17 00:13:01 +0900

    first commit

---
+++ learn-vue3/eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+    rules: {
+      'no-console': 'warn', // files에 적용할 규칙 설정
+    },
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+  {
+    rules: {
+      'no-console': 'warn', // 프로젝트 전체 규칙 설정
+    },
+  },
+]
---
+++ learn-vue3/src/lectures/4/Composition.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>반응형 메세지</h2>
+    <p>{{ reactiveMessage }}</p>
+    <button v-on:click="addMsg">ADD</button>
+    <h2>일반 메세지</h2>
+    <p>{{ normalMessage }}</p>
+    <button v-on:click="addNoMsg">ADD</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const reactiveMessage = ref('Hello')
+    const addMsg = () => {
+      reactiveMessage.value = reactiveMessage.value + '!'
+    }
+    const normalMessage = ref('MNHello')
+    const addNoMsg = () => {
+      reactiveMessage.value = reactiveMessage.value + '!'
+    }
+    return {
+      reactiveMessage,
+      normalMessage,
+      addMsg,
+      addNoMsg,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
