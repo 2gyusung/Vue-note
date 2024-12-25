6dc35f067b4b345beabfb8c6035a32c9b5c79369
이규성 <leegs005@naver.com>
2024-12-25 23:20:31 +0900

    for if vind

---
+++ learn-vue3/src/lectures/11/App_for.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul>
+      <template v-for="(item, idx) in items" :key="item.id">
+        <!-- <li v-if="item.id % 2 == 0">ID : {{ item.id }} 인덱스 : {{ idx }}, {{ item.msg }}</li> -->
+        <li>>ID : {{ item.id }} 인덱스 : {{ idx }}, {{ item.msg }}</li>
+      </template>
+      <!-- <li v-for="(item, idx) in items" :key="item.id">인덱스 : {{ idx }},{{ item.msg }}</li> -->
+      <!-- <li v-for="(item, idx) in items" :key="item.id">인덱스 : {{ idx }},{{ item.msg }}</li> -->
+    </ul>
+    <hr />
+    <ul>
+      <li v-for="(value, key, idx) in myObj" :key="key">{{ idx }}, {{ key }} , {{ value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from 'vue'
+
+export default {
+  setup() {
+    const items = reactive([
+      { id: 1, msg: 'Java' },
+      { id: 2, msg: 'JavaScript' },
+      { id: 3, msg: 'Vue' },
+      { id: 4, msg: 'HTML' },
+    ])
+    const evenItems = computed(() => items.filter(item => item.id % 2 === 0))
+    const myObj = reactive({
+      title: '제목',
+      author: '홍길동',
+      publishedat: '2016-01-01',
+    })
+    return {
+      items,
+      evenItems,
+      myObj,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectures/9/App_vind.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="text" :class="{ active: isActive, 'txt-danger': hasError }">텍스트입니다</div>
+    <button v-on:click="toogle">btn</button>
+    <button v-on:click="hasError = !hasError">btnError</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const isActive = ref(true)
+    const hasError = ref(false)
+    const toggle = () => {
+      isActive.value = !isActive.value
+    }
+    return {
+      isActive,
+      toggle,
+      hasError,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.active {
+  font-weight: 900;
+}
+.txt-danger {
+  color: red;
+}
+</style>
